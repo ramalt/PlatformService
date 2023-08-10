f70662576166f411d3a9505d0dfc05ef070beefb
ramalt <ramazanalltuntepe@gmail.com>
2023-08-10 16:38:22 +0300

    Project dockerized.

---
+++ Controllers/PlatformController.cs
@@ -0,0 +1,60 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformSerivce.Dtos;
+using PlatformSerivce.Models;
+using PlatformSerivce.Repositories;
+
+namespace PlatformSerivce.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformController : ControllerBase
+{
+    private readonly IPlatformRepository _platformRepository;
+    private readonly IMapper _mapper;
+
+    public PlatformController(IPlatformRepository platformRepository, IMapper mapper)
+    {
+        _platformRepository = platformRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<ReadPlatformDto>> GetPlatforms()
+    {
+        var platforms = _platformRepository.GetAllPlatforms();
+
+        //ℹ️ platfroms -> IEnumerable<ReadPlatformDto> 
+        var mapped = _mapper.Map<IEnumerable<ReadPlatformDto>>(platforms);
+
+        return Ok(mapped);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<ReadPlatformDto> GetPlatformById(int id)
+    {
+        var platform = _platformRepository.GetPlatformById(id);
+        if (platform != null)
+        {
+            //ℹ️ platfroms -> IEnumerable<ReadPlatformDto> 
+            var mapped = _mapper.Map<ReadPlatformDto>(platform);
+
+            return Ok(mapped);
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]//🚩returns mapped readPlatformDto type.
+    public ActionResult<ReadPlatformDto> CreatePlatform(CreatePlatformDto createPlatformDto)
+    {
+        var platformModel = _mapper.Map<Platform>(createPlatformDto);
+
+        _platformRepository.CreatePlatform(platformModel);
+        _platformRepository.SaveChanges();
+
+        var created = _mapper.Map<ReadPlatformDto>(platformModel);
+        return Ok(created);
+
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Multistage build: Build environment and runtime environment
+
+# Build environment
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copy everything else and build
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Runtime environment
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS runtime
+WORKDIR /app
+COPY --from=build-env /app/out ./
+ENTRYPOINT ["dotnet", "PlatformSerivce.dll"]
